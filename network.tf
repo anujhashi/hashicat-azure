e84266d93a8a58779af0734e0c9390cbaae9fba3
anujhashi <84107166+anujhashi@users.noreply.github.com>
2022-06-14 14:33:05 -0500

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,9 +1,7 @@
 module "network" {
   source  = "app.terraform.io/hashicorpAnujorg/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "myresourcegroup"
-  variable "resource_group_name" {
-    default     = "myresourcegroup"
+ 
 }
   # insert required variables here
 }
